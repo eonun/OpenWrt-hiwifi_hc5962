3654751e3ba3d19c5e0983d6e03ec742e6797a41
eonun <eonun@qq.com>
2023-04-03 20:59:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,7 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git v2raya_openwrt https://github.com/v2rayA/v2raya-openwrt' feeds.conf.default
+
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
