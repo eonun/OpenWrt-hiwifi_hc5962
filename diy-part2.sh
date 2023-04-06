851b534b46a9e980c3fb920a1f931198578ab3ca
eonun <eonun@qq.com>
2023-04-06 11:30:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
 
-rm -rf  feeds/small/v2raya
-
-rm -rf  feeds/small/luci-app-v2raya
+# rm -rf  feeds/small/v2raya
+# rm -rf  feeds/small/luci-app-v2raya
